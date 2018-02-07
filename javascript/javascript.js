676f306345839c485433efd9a94f514d9451b265
Jameka <meka.jay@live.com>
2018-02-06 22:18:29 -0600

    events feb

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,48 +1,51 @@
 // JavaScript Document
 
 $(document).ready(function (){
+
+
+
 	
 	
-	$(function () {
+// 	$(function () {
 
-      // Slideshow 1
-      $("#slider1").responsiveSlides({
-		width: $(window).width() > 1025 ? 1025 :'auto',
-//        maxwidth: 1025,
-        speed: 800
-      });
+//       // Slideshow 1
+//       $("#slider1").responsiveSlides({
+// 		width: $(window).width() > 1025 ? 1025 :'auto',
+// //        maxwidth: 1025,
+//         speed: 800
+//       });
 
-      // Slideshow 2
-      $("#slider2").responsiveSlides({
-        auto: false,
-        pager: true,
-        speed: 300,
-        maxwidth: 540
-      });
+//       // Slideshow 2
+//       $("#slider2").responsiveSlides({
+//         auto: false,
+//         pager: true,
+//         speed: 300,
+//         maxwidth: 540
+//       });
 
-      // Slideshow 3
-      $("#slider3").responsiveSlides({
-        manualControls: '#slider3-pager',
-        maxwidth: 540
-      });
+//       // Slideshow 3
+//       $("#slider3").responsiveSlides({
+//         manualControls: '#slider3-pager',
+//         maxwidth: 540
+//       });
 
-      // Slideshow 4
-      $("#slider4").responsiveSlides({
-        auto: false,
-        pager: false,
-        nav: true,
-        speed: 500,
-        namespace: "callbacks",
-        before: function () {
-          $('.events').append("<li>before event fired.</li>");
-        },
-        after: function () {
-          $('.events').append("<li>after event fired.</li>");
-        }
-      });
+//       // Slideshow 4
+//       $("#slider4").responsiveSlides({
+//         auto: false,
+//         pager: false,
+//         nav: true,
+//         speed: 500,
+//         namespace: "callbacks",
+//         before: function () {
+//           $('.events').append("<li>before event fired.</li>");
+//         },
+//         after: function () {
+//           $('.events').append("<li>after event fired.</li>");
+//         }
+//       });
 
-    });
+//     });
 	
 	
-});
+// });
  
